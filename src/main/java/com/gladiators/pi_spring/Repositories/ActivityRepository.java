d7b7c18eb326586cbf38145c22d3df769a607937
mohamednakbi <mohamed.nakbi@esprit.tn>
2023-02-20 21:20:10 +0100

    Revert "Crud Gestion Activity"
    This reverts commit c8f7cbce33688fee7086933cc5a44219c32792e8.

--- src/main/java/com/gladiators/pi_spring/Repositories/ActivityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gladiators.pi_spring.Repositories;
-
-import com.gladiators.pi_spring.Entities.Activity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ActivityRepository extends JpaRepository<Activity,Long> {
-}
--- src/main/java/com/gladiators/pi_spring/Repositories/EvaluationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gladiators.pi_spring.Repositories;
-
-import com.gladiators.pi_spring.Entities.Evaluation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EvaluationRepository extends JpaRepository<Evaluation,Long> {
-}
